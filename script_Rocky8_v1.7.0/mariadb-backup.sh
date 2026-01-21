3731f400ca7d7d46bc5f60270eb6114d4e8df12a
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2026-01-21 12:06:15 +0900

    Update MariaDB backup script with new configurations

--- script_Rocky8_v1.7.0/mariadb-backup.sh
+++ script_Rocky8_v1.7.0/mariadb-backup.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
 
+#계정정보
 USER="root"
-PW="Wlfks@09!@#"
+PW="Kbhwang890!@"
 
+#DB Name
 DB_NM="miso"
-EX_TABLE="${DB_NM} mail_send"
-EXCLUDE_1="${DB_NM}.mail_send"
-EXCLUDE_2=""
-EXCLUDE_3=""
-EXCLUDE_4=""
-EXCLUDE_5=""
-EXCLUDE_6=""
+
+#예외 테이블 리스트
+EXCLUDE_LIST=(
+        "${DB_NM}.mail_send"
+        "${DB_NM}.user_action_log"
+        "${DB_NM}.user_login_log"
+)
+EXCLUDE_PATTERN=$(printf "|%s" "${EXCLUDE_LIST[@]}")
+EXCLUDE_PATTERN=${EXCLUDE_PATTERN:1} # 맨 앞의 '|' 제거
+
+#예외 테이블중 구조 다운받을 리스트
+STRUCTURE_ONLY_LIST=(
+        "mail_send"
+        "user_action_log"
+        "user_login_log"
+)
+STRUCTURE_TABLE_STR="${STRUCTURE_ONLY_LIST[*]}"
+MY_CNF="/etc/my.cnf"
+
 CORE=2
 
+#년-월-일
 DATE=$(date +%Y-%m-%d)
-ASDOW=$(TZ=Asia/Seoul date +%w) # 0=Sunday ... 6=Saturday
 
-DATA="/data/mariadbData"
+#요일 확인  0=Sunday ... 6=Saturday
+ASDOW=$(TZ=Asia/Seoul date +%w)
+
+#mariadb Basedir
 MARIADB="/data/mariadb"
-BACKUP_DIR="/data/backup"
+
+#백업경로
+BACKUP_DIR="/backup1"
+
+#로그 경로
 LOG_DIR="$BACKUP_DIR/logs"
-LOG="$LOG_DIR/backup_$DATE.log"
+#로그 파일
+LOG="${LOG_DIR}/backup_$DATE.log"
+#LOG remove day
+LOG_REMOVE_DAYS=3
 
 # 마지막 백업 번호 찾기 (0-6)
-LAST_NUM=$(find /data/backup/INC/ -mindepth 1 -maxdepth 1 -type d -name '[0-6]' 2>/dev/null | \
+LAST_NUM=$(find ${BACKUP_DIR}/INC -mindepth 1 -maxdepth 1 -type d -name '[0-6]' 2>/dev/null | \
            grep -oE '[0-6]$' | sort -n | tail -n1)
+
+# 로그 디렉토리 정리 함수
+function log_dir_remove() {
+    if [ -d "${LOG_DIR}" ]; then
+        echo "log_dir_remove ${LOG_DIR} " >> "$LOG"
+        find ${LOG_DIR}/* -type f -mtime +${LOG_REMOVE_DAYS} -exec rm -rf {} \;
+    fi
+}
 
 # 7일 주기 삭제 함수 (logs 디렉토리 보호)
 function cycle_seven_days_delete(){
@@ -32,6 +64,34 @@ function cycle_seven_days_delete(){
     find "$BACKUP_DIR/INC/" -mindepth 1 -maxdepth 1 -type d ! -name "logs" -exec rm -rf {} +
     echo "Cleanup completed" >> "$LOG"
 }
+
+# 예외 테이블중 구조백업필요한 경우 
+function structure_backup(){
+    # 리스트가 0보다 커야한다. 
+    if [ ${#STRUCTURE_ONLY_LIST[@]} -gt 0 ]; then
+        echo "dumping structure for table: ${STRUCTURE_ONLY_LIST[*]}" >> "${LOG}"
+        "$MARIADB/bin/mariadb-dump" -u"$USER" -p"$PW" \
+                --single-transaction \
+                --lock-tables=false \
+                --no-data \
+                "${DB_NM}" "${STRUCTURE_ONLY_LIST[@]}" > "${BACKUP_DIR}/INC/no_data_structure_tables.sql"
+        if [ $? -eq 0 ]; then
+                    echo "Structure dump successful" >> "${LOG} "
+        else
+                    echo "ERROR: Structure dump failed. Check table names." >> "${LOG}"
+        fi
+    else
+        echo "no specific tables defined for structure backup. skipping" >> "${LOG}"
+    fi
+}
+
+#my.cnf 백업.
+function my_cnf_backup(){
+        if [ -f "${MY_CNF}" ]; then
+                cp ${MY_CNF} "${BACKUP_DIR}/INC/my.cnf"
+        fi
+}
+
 
 # 전체 백업 함수
 function full_backup() {
@@ -44,25 +104,23 @@ function full_backup() {
         --no-lock \
         --user="$USER" \
         --password="$PW" \
-        --tables-exclude="$EXCLUDE_1" \   # --tables-exclude="$EXCLUDE_1 | $EXCLUDE_2" 
+        --tables-exclude="${EXCLUDE_PATTERN}" \
         --target-dir="${BACKUP_DIR}/INC/0/" \
         --binlog-info=ON 2>>"$LOG"
 
+    # mariadb-backup 명령어 $? 성공=0, 실패=1...
     if [ $? -eq 0 ]; then
         echo "Full backup successful" >> "$LOG"
     else
         echo "ERROR: Full backup failed" >> "$LOG"
         exit 1
     fi
-    
+
     # 예외 테이블 DDL 백업
-    "$MARIADB/bin/mariadb-dump" -u"$USER" -p"$PW" \
-        --single-transaction \
-        --lock-tables=false \
-        --no-data $EX_TABLE > "${BACKUP_DIR}/INC/${EXCLUDE_1}_structure.sql"
+    structure_backup
 
     # my.cnf 백업
-    cp /etc/my.cnf "${BACKUP_DIR}/INC/my.cnf"
+    my_cnf_backup
 
     echo "Full Backup Completed: $DATE" >> "$LOG"
     echo "========================================" >> "$LOG"
@@ -70,7 +128,9 @@ function full_backup() {
 
 # 증분 백업 함수
 function incremental_backup() {
+    # 앞전 마지막으로 백업한 베이스 디렉토리
     local base_dir="$1"
+    # 현재 백업할 디렉토리
     local target_dir="$2"
 
     echo "========================================" >> "$LOG"
@@ -83,11 +143,12 @@ function incremental_backup() {
         --no-lock \
         --user="$USER" \
         --password="$PW" \
-        --tables-exclude="$EXCLUDE_1" \
+        --tables-exclude="${EXCLUDE_PATTERN}" \
         --incremental-basedir="$base_dir" \
         --target-dir="$target_dir" \
         --binlog-info=ON 2>>"$LOG"
 
+    # mariadb-backup 명령어 $? 성공=0, 실패=1...
     if [ $? -eq 0 ]; then
         echo "Incremental backup successful" >> "$LOG"
     else
@@ -101,6 +162,7 @@ function incremental_backup() {
 
 # 디렉토리 확인 및 생성
 function check_create_dir(){
+# 파리미터 로 받은 패스가 디렉토리가 아니면 생성.
     if [ ! -d "$1" ]; then
         mkdir -p "$1"
         echo "Created directory: $1" >> "$LOG"
@@ -131,10 +193,10 @@ case $ASDOW in
         full_backup
         ;;
 
-    1|2|3|4|5|6)  # Monday to Saturday - Incremental Backup
+    1|2|3|4|5|6)  # 월요일(1) ... 토요일(6) - 증분백업
         if [ -z "$LAST_NUM" ]; then
             # 이전 백업이 없으면 전체 백업 수행
-            echo "No previous backup found. Performing full backup." >> "$LOG"
+            echo "previous backup not found. Performing full backup." >> "$LOG"
             cycle_seven_days_delete
             check_create_dir "${BACKUP_DIR}/INC/0"
             full_backup
@@ -168,5 +230,3 @@ esac
 echo "Backup process finished at: $(date +%Y-%m-%d\ %H:%M:%S)" >> "$LOG"
 echo "========================================" >> "$LOG"
 echo "" >> "$LOG"
-
-
