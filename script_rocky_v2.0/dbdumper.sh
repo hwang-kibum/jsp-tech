4d8c4cfbb02e96e046ad4331f97840f6b861539c
lsm97 <leesang7016@gmail.com>
2026-06-12 11:27:33 +0900

    Update dbdumper.sh

--- script_rocky_v2.0/dbdumper.sh
+++ script_rocky_v2.0/dbdumper.sh
@@ -16,10 +16,9 @@ BACKUP_DIR="/data/backup"
 
 #예외 테이블 리스트
 EXCLUDE_LIST=(
-        miso3.user_bak    #스키마 없음
-		miso_restore.user #실제파일
         miso.user_backup  #테이블 없음
-		miso.user         #실제 파일
+		miso.user_login_log         #실제 파일
+		miso.user_action_log
 		
 )
 MY_CNF="/etc/my.cnf"
@@ -33,13 +32,14 @@ chkp=$(cat /etc/mydumper.cnf 2>/dev/null | grep -i "\[client\]")
 if [ -z "$chkp" ]; then
 	mkcnf
 fi
-	mkdir -p "$BACKUP_DIR"/"$DATE"/backup
+	mkdir -p "$BACKUP_DIR"/"$DATE"/backup_struct
+	mkdir -p "$BACKUP_DIR"/"$DATE"/backup_data
 	mkdir -p "$BACKUP_DIR"/"$DATE"/log
-	mkdir -p "$BACKUP_DIR"/"$DATE"/exclude_tables
-> "$BACKUP_DIR/$DATE/log/mydumper.log"
+> "$BACKUP_DIR/$DATE/log/mydumper_struct.log"
+> "$BACKUP_DIR/$DATE/log/mydumper_data.log"
 > "$BACKUP_DIR/$DATE/skip-tables.txt"
 printf "%s\n" "${EXCLUDE_LIST[@]}" >> "$BACKUP_DIR"/"$DATE"/skip-tables.txt 
-
+################################## struct
 systemd-run \
 --scope \
 --unit=mydumper-final \
@@ -54,39 +54,43 @@ mydumper \
 $( [ "$DB_NM" != "all" ] && echo "--database=$DB_NM" ) \
 --threads=4 \
 --rows=50000 \
+--regex '^(?!(information_schema\.|performance_schema\.|sys\.))' \
+--outputdir="$BACKUP_DIR"/"$DATE"/backup_struct \
+--no-data \
+--compress \
+--trx-tables=0 \
+--set-names=utf8mb4 \
+--events --triggers \
+--logfile="$BACKUP_DIR"/"$DATE"/log/mydumper_struct.log \
+--verbose=3 \
+--clear
+################################## data  
+systemd-run \
+--scope \
+--unit=mydumper-final \
+-p CPUQuota=200% \
+-p CPUWeight=50 \
+-p MemoryHigh=1G \
+-p MemoryMax=2G \
+-p MemorySwapMax=0 \
+-p IOWeight=50 \
+mydumper \
+--defaults-file=/etc/mydumper.cnf \
+$( [ "$DB_NM" != "all" ] && echo "--database=$DB_NM" ) \
+--no-schemas \
+--threads=4 \
+--rows=50000 \
 --omit-from-file="$BACKUP_DIR"/"$DATE"/skip-tables.txt  \
 --regex '^(?!(information_schema\.|performance_schema\.|sys\.))' \
---outputdir="$BACKUP_DIR"/"$DATE"/backup \
+--outputdir="$BACKUP_DIR"/"$DATE"/backup_data \
 --compress \
 --trx-tables=0 \
 --set-names=utf8mb4 \
 --events --routines --triggers \
---logfile="$BACKUP_DIR"/"$DATE"/log/mydumper.log \
+--logfile="$BACKUP_DIR"/"$DATE"/log/mydumper_data.log \
 --verbose=3 \
---clear
-
-if [ $? -ne 0 ]; then
-    echo "check "$BACKUP_DIR"/"$DATE"/log/mydumper.log"
-	exit 1
-fi
-
-backup_schema_names=$( find "$BACKUP_DIR"/"$DATE"/backup -type f -name '*-schema-create.sql.gz' | sed 's#.*/##' | sed 's/-schema-create\.sql\.gz$//' )
-TABLES=$(
-while read table; do
-    schema=${table%%.*}
-
-    if echo "$backup_schema_names" | grep -qx "$schema"; then
-        echo "$table"
-    fi
-done < "$BACKUP_DIR/$DATE/skip-tables.txt" | paste -sd,
-)
-
-mydumper \
-  --tables-list="$TABLES" \
-  --no-data \
-  --outputdir="$BACKUP_DIR"/"$DATE"/exclude_tables \
-  --logfile="$BACKUP_DIR"/"$DATE"/log/mydumper_exclude.log \
-  --clear
+--clear  
+  
 }
 
 loader() {
@@ -104,27 +108,24 @@ loader() {
 		check_date=$(find "$BACKUP_DIR" -mindepth 1 -maxdepth 1 -type d | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}$' | sort | tail -1)
 	fi
 	mkdir -p "$BACKUP_DIR"/"$DATE"/log
-	> "$BACKUP_DIR/$DATE/log/myloader.log"
-	### 예외테이블 먼저 백업 (예외테이블이 view 테이블을 참조 할수도 있음)
+	> "$BACKUP_DIR/$DATE/log/myloader_struct.log"
+	> "$BACKUP_DIR/$DATE/log/myloader_data.log"
+	echo "start recover $check_date"
 	myloader \
   --defaults-file=/etc/mydumper.cnf \
-  --directory="$BACKUP_DIR"/"$check_date"/exclude_tables \
-  --threads=4 \
-  --logfile="$BACKUP_DIR"/"$DATE"/log/myloader_exclude.log 
-		
-	myloader \
-  --defaults-file=/etc/mydumper.cnf \
-  --directory="$BACKUP_DIR"/"$check_date"/backup \
+  --directory="$BACKUP_DIR"/"$check_date"/backup_struct \
   --threads=4 \
   --drop-database \
-  --drop-table \
-  --logfile="$BACKUP_DIR"/"$DATE"/log/myloader.log 
+  --verbose=3 \
+  --logfile="$BACKUP_DIR"/"$DATE"/log/myloader_struct.log 
+	echo "struct recover done"
 	
-	if [ $? -ne 0 ]; then
-		echo "check "$BACKUP_DIR"/"$DATE"/log/myloader.log"
-		exit 1
-	fi	
-	
+	myloader \
+  --defaults-file=/etc/mydumper.cnf \
+  --directory="$BACKUP_DIR"/"$check_date"/backup_data \
+  --threads=4 \
+  --verbose=3 \
+  --logfile="$BACKUP_DIR"/"$DATE"/log/myloader_data.log 
 
 }
 
